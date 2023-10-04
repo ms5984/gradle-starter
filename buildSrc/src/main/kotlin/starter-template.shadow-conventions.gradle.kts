60d2a3a9f5e3b202fa1df9a414a23e3729843d39
Matt <58913953+ms5984@users.noreply.github.com>
2023-10-04 00:27:42 -0400

    fix(shadow): remove broken code
    The check is not reliable in downstream projects. We will just leave it commented out.

--- buildSrc/src/main/kotlin/starter-template.shadow-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/starter-template.shadow-conventions.gradle.kts
@@ -1,13 +1,6 @@
 plugins {
     id("starter-template.java-conventions")
+//    // Shadow must be on the classpath for the shadow plugin to be applied.
+//    // Make sure shadow is uncommented in the dependencies block of buildSrc/build.gradle.kts.
+//    id("com.github.johnrengelman.shadow")
 }
-
-val missing = buildscript.configurations["classpath"].allDependencies.stream()
-    .noneMatch { it.group == "com.github.johnrengelman" && it.name == "shadow" }
-if (missing) {
-    """
-        The shadow plugin isn't on the convention plugin classpath.
-        Make sure it's uncommented in the dependencies block of buildSrc/build.gradle.kts
-    """.trimIndent().let(::error)
-}
-apply(plugin = "com.github.johnrengelman.shadow")
